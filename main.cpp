c0672b6dbd096fd535a20917707e3dfbae1bf332
Ting-Yuan Hsia <ly2314@gmail.com>
2013-02-25 09:58:04 +0800

    20130225

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QtGui/QApplication>
+#include "painter.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Painter w;
+    w.show();
+    
+    return a.exec();
+}
